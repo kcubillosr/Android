8bb77ecc96a3b3a0692d8be546b1598d3cd530e0
Daniel Tapia <dtapia1025@gmail.com>
2018-11-19 21:11:00 -0300

    Commit 1 Proyecto Android

--- app/src/main/java/com/example/daniel/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/daniel/projectandroid/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.daniel.projectandroid;
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +19,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private EditText nombreProducto, descripcion, precio;
+    private double longitud, latitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,52 @@ public class MainActivity extends AppCompatActivity {
         nombreProducto = (EditText) findViewById(R.id.editText);
         descripcion = (EditText) findViewById(R.id.editText2);
         precio = (EditText) findViewById(R.id.editText3);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion localizacion = new Localizacion();
+        localizacion.setMainActivity(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) localizacion);
+    }
+
+    public class Localizacion implements LocationListener{
+        MainActivity mainActivity;
+
+        public void setMainActivity(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        public MainActivity getMainActivity() {
+            return mainActivity;
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            longitud = location.getLongitude();
+            latitud = location.getLatitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
     }
 
     public void insertar(View view){
@@ -34,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
             String nombre, descri;
             int preci;
+            Toast.makeText(this, "El valor en longitud es: " + longitud + " y el valor en latitud es: " + latitud, Toast.LENGTH_SHORT).show();
             nombre = nombreProducto.getText().toString();
             descri = descripcion.getText().toString();
             preci = Integer.parseInt(precio.getText().toString());
@@ -46,6 +101,8 @@ public class MainActivity extends AppCompatActivity {
                 objectDV.put("nombre", nombre);
                 objectDV.put("descripcion", descri);
                 objectDV.put("precio", preci);
+                objectDV.put("longituds", longitud);
+                objectDV.put("latituds", latitud);
 
                 long nfilas = db.insert("tbl_productos", null, objectDV);
 
--- app/src/main/java/com/example/daniel/projectandroid/dbHelper.java
+++ app/src/main/java/com/example/daniel/projectandroid/dbHelper.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class dbHelper extends SQLiteOpenHelper {
-    private String query = "CREATE TABLE tbl_productos (nombre TEXT, descripcion TEXT, precio INTEGER";
+    private String query = "CREATE TABLE tbl_productos (nombre TEXT, descripcion TEXT, precio INTEGER, longituds REAL, latituds REAL)";
     public dbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
